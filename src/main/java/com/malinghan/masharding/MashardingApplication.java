4be1e280db2b42b8519d5e59026845d118a8964c
mattma <linghan.ma@gmail.com>
2026-03-01 19:44:43 +0800

    init

---
+++ src/main/java/com/malinghan/masharding/MashardingApplication.java
@@ -0,0 +1,13 @@
+package com.malinghan.masharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MashardingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MashardingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/malinghan/masharding/MashardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malinghan.masharding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MashardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
